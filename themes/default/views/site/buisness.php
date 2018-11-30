e8687c8065b3c987b49a35ff84e8f454bfaa3a99
schevgeny <schevgeny@gmail.com>
2018-11-30 15:29:57 +0300

    fix

--- themes/default/views/site/buisness.php
+++ themes/default/views/site/buisness.php
@@ -32,7 +32,7 @@ $serviceForm = [];
         <div class="row">
             <div class="col-xl-8 col-lg-8 col-md-7 col-sm-12 col-12">
                 <div class="vendor-details">
-                    <? // $this->render('_vendor_info'); ?>
+                    <?php // $this->render('_vendor_info'); ?>
                     <img src="/uploads/company/<?= $model->image; ?>" alt="" class="img-fluid">
                     <div class="vendor-descriptions">
                         <h3 class="border-bottom mb20 pdb10">About Company</h3>
