9ad3a6e8190a5290f0e2f3d14c37e9cc17a51f73
«Shod» <oleg.shmyk@gmail.com>
2024-11-01 09:47:06 +0100

    Country

--- config/params.php
+++ config/params.php
@@ -2,6 +2,6 @@
 
 return array_merge([
     'adminEmail' => 'sales@yachtservice.vip',
-    'country' => 84,
+    'country' => 152,
     'cdn_url' => 'https://www.static.yachtservice.vip',
 ], require(__DIR__ . '/components/params.php'));
